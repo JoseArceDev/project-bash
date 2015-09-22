a7296babae2a0a9e656de996a6ac32db61e66a81
JoseArceDev <jose.arce.dev@gmail.com>
2015-09-22 16:15:53 -0500

    First commit

---
+++ app/src/main/java/com/koombea/sensortest/TestService.java
@@ -0,0 +1,133 @@
+package com.koombea.sensortest;
+
+import android.app.Notification;
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.PowerManager;
+import android.os.Process;
+import android.os.Vibrator;
+import android.util.Log;
+import android.widget.Toast;
+
+/**
+ * Created by koombea on 6/23/15.
+ */
+public class TestService extends Service implements SensorEventListener {
+    public static final String TAG = TestService.class.getName();
+    public static final int SCREEN_OFF_RECEIVER_DELAY = 500;
+
+    private SensorManager mSensorManager = null;
+    private PowerManager.WakeLock mWakeLock = null;
+
+    /*
+     * Register this as a sensor event listener.
+     */
+    private void registerListener() {
+        mSensorManager.registerListener(this,
+                mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+                SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    /*
+     * Un-register this as a sensor event listener.
+     */
+    private void unregisterListener() {
+        mSensorManager.unregisterListener(this);
+    }
+
+    public BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.i(TAG, "onReceive(" + intent + ")");
+
+            if (!intent.getAction().equals(Intent.ACTION_SCREEN_OFF)) {
+                return;
+            }
+
+            Runnable runnable = new Runnable() {
+                public void run() {
+                    Log.i(TAG, "Runnable executing.");
+                    unregisterListener();
+                    registerListener();
+                }
+            };
+
+            new Handler().postDelayed(runnable, SCREEN_OFF_RECEIVER_DELAY);
+        }
+    };
+
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        Log.i(TAG, "onAccuracyChanged().");
+    }
+
+    /* put this into your activity class */
+    private float mAccel; // acceleration apart from gravity
+    private float mAccelCurrent; // current acceleration including gravity
+    private float mAccelLast; // last acceleration including gravity
+
+    public void onSensorChanged(SensorEvent event) {
+        Log.i(TAG, "onSensorChanged().");
+
+        float x = event.values[0];
+        float y = event.values[1];
+        float z = event.values[2];
+        mAccelLast = mAccelCurrent;
+        mAccelCurrent = (float) Math.sqrt((double) (x*x + y*y + z*z));
+        float delta = mAccelCurrent - mAccelLast;
+        mAccel = mAccel * 0.9f + delta; // perform low-cut filter
+
+        if (mAccel > 12) {
+            Toast.makeText(this, "Device has shaken.", Toast.LENGTH_LONG).show();
+            Vibrator v = (Vibrator) this.getSystemService(Context.VIBRATOR_SERVICE);
+            v.vibrate(500);
+        }
+
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        PowerManager manager =
+                (PowerManager) getSystemService(Context.POWER_SERVICE);
+        mWakeLock = manager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, TAG);
+
+        registerReceiver(mReceiver, new IntentFilter(Intent.ACTION_SCREEN_OFF));
+
+    }
+
+    @Override
+    public void onDestroy() {
+        unregisterReceiver(mReceiver);
+        unregisterListener();
+        mWakeLock.release();
+        stopForeground(true);
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        super.onStartCommand(intent, flags, startId);
+
+        startForeground(Process.myPid(), new Notification());
+        registerListener();
+        mWakeLock.acquire();
+
+        return START_STICKY;
+    }
+}
